8a7e841f8d797bb588a5820af7275d7906a0f9bc
Aleksandra <aleksandra.tuszy@gmail.com>
2022-05-03 22:05:28 +0200

    new features

--- index.js
+++ index.js
@@ -16,12 +16,13 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.getElementById("q").style.display="none";
     document.getElementById("myposts-btn").style.display="block";
     document.getElementById("mypets-btn").style.display="block";
+    document.getElementById("mojekonto").style.display="block";
 
     if(user != null){
      
      //console.log("1st: "+uid);
      //console.log("1st: "+email_id);
-      userdata1();
+    userdata1();
       //user.sendEmailVerification();????
     }
 
@@ -34,6 +35,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.getElementById("q").style.display="block";
     document.getElementById("myposts-btn").style.display="none";
     document.getElementById("mypets-btn").style.display="none";
+    document.getElementById("mojekonto").style.display="none";
   }
 });
 
@@ -195,4 +197,138 @@ function checkUserPassword(){
     }else{
       //  document.getElementById("userPasswordError").style.display = "none";
     }
+}
+
+
+// Filter table
+
+$(document).ready(function(){
+  $("#tableSearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+// //Filter list
+
+// $(document).ready(function(){
+//   $("#listSearch").on("keyup", function() {
+//     var value = $(this).val().toLowerCase();
+//     $("#myList li").filter(function() {
+//       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//     });
+//   });
+// });
+
+// FIlter anything
+$(document).ready(function(){
+  $("#myPetsinput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myPets button").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+function mojezwierzeta(){
+  document.getElementById('myPets').innerHTML="";
+  document.getElementById('myPetsinput').style.display = 'flex';
+  firebase.database().ref('users/'+firebase.auth().currentUser.uid+'/pets/').once('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+    {
+    // document.getElementById("imie").value=childSnapshot.val().name;
+     //document.getElementById("nazwisko").value=childSnapshot.val().lastname;
+     //console.log(childSnapshot.val().name +" " + childSnapshot.val().type);
+     var lista = document.getElementById('myPets');
+     var newpet = document.createElement('button');
+     newpet.setAttribute('class', 'list-group list-group-flush list-group-item');
+     var typ =  childSnapshot.val().type;
+     var col;
+     if(typ == 'psy') col = 'teal';
+     else if(typ == 'koty') col = 'purple';
+     else if(typ =='gryzonie') col = 'pink';
+     else col = 'gray';
+     newpet.style.display='flex';
+     newpet.style.alignItems='center';
+     newpet.style.justifyContent='space-between';
+     newpet.style.fontSize='20px';
+     newpet.innerHTML='<span class="tag tag-'+col+'">'+ childSnapshot.val().type+'</span><p>'+childSnapshot.val().name+'</p>';
+     newpet.setAttribute('id',childSnapshot.key);
+     newpet.setAttribute('value', childSnapshot.val().name);
+     newpet.setAttribute('onclick','ogolne(this.id, this.value)');
+     
+     lista.appendChild(newpet);     
+    }
+
+  }); });
+  
+}
+function dodajzwierze(){
+  var types = document.forms[0];
+  var selected;
+  for (i = 0; i < types.length; i++) {
+    if (types[i].checked) {
+      selected = types[i].value;
+    }
+    }
+    
+
+  firebase.database().ref('/users/' + firebase.auth().currentUser.uid+'/pets/').push({
+    name: document.getElementById("petname").value,
+    type: selected
+   });
+   mojezwierzeta();
+    // window.alert("Zapisano."); 
+}
+function ogolne(pet_uid,pet_name){
+  localStorage.setItem('pet_uid',pet_uid);
+  //console.log("ogolne "+pet_uid);
+  document.getElementById("namediv").innerText=pet_name;
+  var rasa = document.getElementById("rasa");
+  var umaszczenie = document.getElementById("umaszczenie");
+  var charakter = document.getElementById("charakter");
+  var upodobania = document.getElementById("upodobania");
+  var dodinfo = document.getElementById("dodinfo");
+}
+function histmed(){
+  var opis1 = document.getElementById("opis").value;
+  var zabiegi1 = document.getElementById("zabiegi").value;
+  var zalecenia1 = document.getElementById("zalecenia").value;
+  var pet_uid = localStorage.getItem('pet_uid');
+  var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+  
+  
+  firebase.database().ref('/users/' + firebase.auth().currentUser.uid+'/pets/'+pet_uid+"/Med/").push({
+    opis: opis1,
+    zabiegi: zabiegi1,
+    zalecenia: zalecenia1,
+    data: dateTime
+   });
+}
+function pokazhistmed(){
+  document.getElementById('poprzedniemed').style.display="block";
+  document.getElementById('poprzedniemed-list').innerHTML="";
+var pet_uid = localStorage.getItem('pet_uid');
+  firebase.database().ref('users/'+firebase.auth().currentUser.uid+'/pets/'+pet_uid+"/Med/").once('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+    {
+     var lista = document.getElementById('poprzedniemed-list');
+     var newpet = document.createElement('button');
+     newpet.setAttribute('class', 'list-group list-group-flush list-group-item');
+     
+     newpet.style.display='flex';
+     newpet.style.alignItems='center';
+     newpet.style.justifyContent='space-between';
+     newpet.style.fontSize='20px';
+     newpet.innerHTML='<ul><li> Opis: '+childSnapshot.val().opis+'</li>'+'<li> Zabiegi: '+childSnapshot.val().zabiegi+'</li>'+'<li>Zalecenia: '+childSnapshot.val().zalecenia+'</li><li>Data dodania: '+childSnapshot.val()?.data+'</li></ul>';
+     newpet.setAttribute('id',childSnapshot.key);
+     
+     lista.appendChild(newpet);     
+    }
+
+  }); });
 }
